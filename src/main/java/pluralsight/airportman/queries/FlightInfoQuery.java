1a55c562d6f0e1e2a4275f11e6b5e9ea05dd234d
tmnhat <trinhminhnhat2011@gmail.com>
2020-07-30 18:05:43 +0700

    add TODO

--- src/main/java/pluralsight/airportman/queries/FlightInfoQuery.java
+++ src/main/java/pluralsight/airportman/queries/FlightInfoQuery.java
@@ -11,5 +11,15 @@ public class FlightInfoQuery {
         this.mongoTemplate = mongoTemplate;
     }
 
-    
+    /**
+     * TODO: implement all the following queries
+     *  1. Find all flight with paging and sorting
+     *  2. Find the flight by ID
+     *  3. Count all international flights
+     *  4. Find all flight by departure city
+     *  5. Find all flight by duration between min and max
+     *  6. Find all flight delayed at a particular departure city
+     *  7. Find all flight that are on time and relate to a city
+     *  8. Find by aircraft model
+     */
 }
